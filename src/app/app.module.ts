d38159625f7edfc263a4c5905d534f0e581bdf3b
Saulo <saulo.manograsso@gmail.com>
2018-09-13 17:20:25 -0300

    primer version intgrada con boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UiModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
